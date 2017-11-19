dc04258d2111dd99e18e8d6e959088bc28195bad
Justin Chau <justin.chau623@gmail.com>
2017-11-19 02:53:52 -0800

    (IntelliJ isn't committing all my code at once, okay...)
    Variable assignments are now entered into the function after desugaring. e.g. c1 = make(D) -> c1 = make(c1, D)
    
    Functions are being called now in the interpreter
    
    Some further refactoring

--- src/ming/cpsc311/ubc/Main.java
+++ src/ming/cpsc311/ubc/Main.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class Main {
@@ -21,48 +22,42 @@ public class Main {
     // 6. uninstantiated variables                                  TODO
     // 7. naming variables funciton names eg bond = make('C')       TODO
 
-    public static void main(String[] args) throws FileNotFoundException, InvalidFunctionException {
+    public static void main(String[] args) throws IOException, InvalidFunctionException, NoSuchMethodException {
 	    if (args.length != 1) {
 	        throw new IllegalArgumentException("Only one argument can be passed in, a MING file");
         }
 
-        try{
-            //changed this because before input would be split on commas
-            //buffered reader reads on each line, doesnt split on comma
-            BufferedReader in = new BufferedReader(new FileReader(args[0]));
-            String readLine;
-            //for writing to file
-            // what do we save this into a file for?
-            FileWriter fw = new FileWriter("tmp.java");
-            BufferedWriter writer = new BufferedWriter(fw);
-            ArrayList<String> parsed = new ArrayList<>();
+        //changed this because before input would be split on commas
+        //buffered reader reads on each line, doesnt split on comma
+        BufferedReader in = new BufferedReader(new FileReader(args[0]));
+        String readLine;
+        ArrayList<String> parsed = new ArrayList<>();
+        InputParser parser = new InputParser();
 
-            InputParser parser = new InputParser();
-
-            while ((readLine = in.readLine()) != null) {
-                //parsing then adding to 
-                parsed.add(parser.parse(readLine.trim()));
-            }
-
-            for (String line: parsed){
-                //writing to file
-                writer.write(line);
-                writer.newLine();
-            }
-            writer.flush();
-            writer.close();
-            fw.close();
-
-            ArrayList<Structure> stuff = parser.getAll();
-
-            for(Structure x: stuff){
-                for(Bond y: x.getBonds()){
-                    System.out.println(y.getBondType());
-                    System.out.println(y.getStructure());
-                }
-            }
-        } catch (IOException e){
-            e.printStackTrace();
+        while ((readLine = in.readLine()) != null) {
+            //parsing then adding to
+            parsed.add(parser.parse(readLine.trim()));
         }
+
+        //for writing to file (for demonstrating desugaring)
+        ParsedMINGToJavaWriter writer = new ParsedMINGToJavaWriter("tmp.java");
+        writer.write(parsed);
+
+        // Interpretation:
+        InputInterpreterInterface interpreter = new InputInterpreter();
+        // "reflection" - https://stackoverflow.com/questions/160970/how-do-i-invoke-a-java-method-when-given-the-method-name-as-a-string
+        interpreter.interpret(parsed);
+
+
+        /*
+        ArrayList<Structure> stuff = parser.getAll();
+
+        for(Structure x: stuff){
+            for(Bond y: x.getBonds()){
+                System.out.println(y.getBondType());
+                System.out.println(y.getStructure());
+            }
+        }
+        */
     }
 }
---
+++ src/ming/cpsc311/ubc/ParsedMINGToJavaWriter.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+class ParsedMINGToJavaWriter {
+
+    private FileWriter fw;
+    private BufferedWriter writer;
+
+    ParsedMINGToJavaWriter(String filepath) throws IOException {
+        this.fw = new FileWriter(filepath);
+        this.writer = new BufferedWriter(fw);
+    }
+    void write(ArrayList<String> parsed) throws IOException {
+        for (String line: parsed){
+            //writing to file
+            writer.write(line);
+            writer.newLine();
+        }
+        writer.flush();
+        writer.close();
+        fw.close();
+    }
+}
