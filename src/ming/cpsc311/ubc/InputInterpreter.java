59490bf47fe3b4709201bae5459dfca2b5f2e5ed
Justin Chau <justin.chau623@gmail.com>
2017-11-19 02:57:59 -0800

    (IntelliJ isn't committing all my code at once, okay...)
    Variable assignments are now entered into the function after desugaring. e.g. c1 = make(D) -> c1 = make(c1, D)
    
    Functions are being called now in the interpreter
    
    Some further refactoring

--- src/ming/cpsc311/ubc/InputInterpreter.java
+++ src/ming/cpsc311/ubc/InputInterpreter.java
@@ -1,4 +1,38 @@
+import Exceptions.InvalidFunctionException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class InputInterpreter implements InputInterpreterInterface{
+    HashMap<String, Structure> structures = new HashMap<>();
+
+    @Override
+    public void interpret(ArrayList<String> commands) throws NoSuchMethodException, InvalidFunctionException {
+        Method currentMethod = null;
+        String vName;
+        String function;
+        for(String s : commands) {
+            if (s.contains("=")) {
+                String[] arr = s.split("=");
+                vName = InputParser.stripOutStructure(InputParser.sanitize(arr[0]));
+                function = InputParser.sanitize(arr[1]);
+            } else {
+                vName = null;
+                function = InputParser.sanitize(s);
+            }
+            String[] f = function.split("\\(");
+            String functionName = f[0].toLowerCase();
+            String[] arguments = InputParser.obtainArguments(f[1]);
+            Method method = obtainMethod(functionName);
+            try {
+                method.invoke(this, (Object[]) arguments);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     /**
      * Creates a Structure from one element
@@ -14,22 +48,24 @@ public class InputInterpreter implements InputInterpreterInterface{
 
     /**
      * Creates a structure of a carbon ring
-     *
-     * @param n Number of carbons in the ring
-     * @param b TODO
+     * @param vName String of variable ring is assigned to
+     * @param n_string Number of carbons in the ring
+     * @param b_string TODO
      * @return Structure
      */
-    public Structure make_ring(int n, boolean b) {
+    public Structure make_ring(String vName, String n_string, String b_string) {
+        int n = Integer.parseInt(n_string);
+        Boolean b = Boolean.valueOf(b_string);
         return null;
     }
 
     /**
      * Creates a chain of elements
-     *
+     * @param vName String of variable chain is assigned to
      * @param s Element used to create the chain
      * @return Structure
      */
-    public Structure make_chain(String s) {
+    public Structure make_chain(String vName, String s) {
         return null;
     }
 
@@ -38,12 +74,12 @@ public class InputInterpreter implements InputInterpreterInterface{
      *
      * @param s1        The first structure
      * @param s2        The second structure
-     * @param index_1   The index of the element in s1 that will be joined to s2
-     * @param index_2   The index of the element in s2 that will be joined to s1
-     * @param bond_type Denotes single/double/triple/in/out bond
+     * @param bond_type_string Denotes single/double/triple/in/out bond
      * @return Structure
      */
-    public Structure join(Structure s1, Structure s2, int index_1, int index_2, int bond_type) {
+    //TODO: Look into hashmap for the corresponding Structure!
+    public Structure join(String s1, String s2, String bond_type_string) {
+        int bond_type = Integer.parseInt(bond_type_string);
         return null;
     }
 
@@ -53,7 +89,8 @@ public class InputInterpreter implements InputInterpreterInterface{
      * @param s The structure to be mirrored
      * @return Structure
      */
-    public Structure mirror(Structure s) {
+    //TODO: Look into hashmap for the corresponding Structure!
+    public Structure mirror(String s) {
         return null;
     }
 
@@ -63,7 +100,8 @@ public class InputInterpreter implements InputInterpreterInterface{
      * @param s Structure to be converted to SMILES
      * @return String
      */
-    public String toSmiles(Structure s) {
+    //TODO: Look into hashmap for the corresponding Structure!
+    public String toSmiles(String s) {
         return null;
     }
 
@@ -73,7 +111,8 @@ public class InputInterpreter implements InputInterpreterInterface{
      * @param s Structure to be checked
      * @return boolean
      */
-    public boolean checkValidity(Structure s) {
+    //TODO: Look into hashmap for the corresponding Structure!
+    public boolean checkValidity(String s) {
         return false;
     }
 
@@ -82,7 +121,45 @@ public class InputInterpreter implements InputInterpreterInterface{
      *
      * @param s Structure to be rendered
      */
-    public void render(Structure s) {
+    //TODO: Look into hashmap for the corresponding Structure!
+    public void render(String s) {
 
+    }
+
+    private Method obtainMethod(String function) throws NoSuchMethodException {
+        Method currentMethod;
+        try {
+            switch (function) {
+                case "make":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class, String.class);
+                    break;
+                case "make_ring":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class, String.class, String.class);
+                    break;
+                case "make_chain":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class, String.class);
+                    break;
+                case "join":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class, String.class, String.class);
+                    break;
+                case "mirror":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class);
+                    break;
+                case "toSmiles":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class);
+                    break;
+                case "checkValidity":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class);
+                    break;
+                case "render":
+                    currentMethod = InputInterpreter.class.getMethod(function, String.class);
+                    break;
+                default:
+                    throw new NoSuchMethodException(function + " does not exist");
+            }
+        } catch (NoSuchMethodException e) {
+            throw new NoSuchMethodException(function + " does not exist");
+        }
+        return currentMethod;
     }
 }
