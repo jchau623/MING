b36093772cdc5984ddfd58f0c0a3b5d7bb7b837a
Justin Chau <justin.chau623@gmail.com>
2017-11-19 02:55:43 -0800

    (IntelliJ isn't committing all my code at once, okay...)
    Variable assignments are now entered into the function after desugaring. e.g. c1 = make(D) -> c1 = make(c1, D)
    
    Functions are being called now in the interpreter
    
    Some further refactoring

--- src/ming/cpsc311/ubc/InputInterpreterInterface.java
+++ src/ming/cpsc311/ubc/InputInterpreterInterface.java
@@ -1,61 +1,12 @@
+import Exceptions.InvalidFunctionException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public interface InputInterpreterInterface {
     /**
-     * Creates a Structure from one element
-     * @param s String of element to be created
-     * @return Structure
+     * Entry-point for the interpreter
+     * @param commands List of desugared MING instructions
      */
-    Structure make(String s);
-
-    /**
-     * Creates a structure of a carbon ring
-     * @param n Number of carbons in the ring
-     * @param b TODO
-     * @return Structure
-     */
-    Structure make_ring(int n, boolean b);
-
-    /**
-     * Creates a chain of elements
-     * @param s Element used to create the chain
-     * @return Structure
-     */
-    Structure make_chain(String s);
-
-    /**
-     * Returns a Structure from joining two Structures together
-     * @param s1 The first structure
-     * @param s2 The second structure
-     * @param index_1 The index of the element in s1 that will be joined to s2
-     * @param index_2 The index of the element in s2 that will be joined to s1
-     * @param bond_type Denotes single/double/triple/in/out bond
-     * @return Structure
-     */
-    Structure join(Structure s1, Structure s2, int index_1, int index_2, int bond_type);
-
-    /**
-     * Produces the enantiomer of the given structure
-     * @param  s The structure to be mirrored
-     * @return Structure
-     */
-    Structure mirror(Structure s);
-
-    /**
-     * Converts MING Structure to SMILES notation
-     * @param s Structure to be converted to SMILES
-     * @return String
-     */
-    String toSmiles(Structure s);
-
-    /**
-     * Checks validity of the given MING Structure
-     * @param s Structure to be checked
-     * @return boolean
-     */
-    boolean checkValidity(Structure s);
-
-    /**
-     * Renders the structure given
-     * @param s Structure to be rendered
-     */
-    void render(Structure s);
+    void interpret(ArrayList<String> commands) throws NoSuchMethodException, InvalidFunctionException;
 }
--- src/ming/cpsc311/ubc/InputParser.java
+++ src/ming/cpsc311/ubc/InputParser.java
@@ -9,6 +9,8 @@ class InputParser {
 
     // private ArrayList<Structure> elements = new ArrayList<>();
 
+    // Global variables are cheating, I'm sorry! But it's pretty late..
+    private String variableName;
     String parse(String s) throws InvalidFunctionException {
         String func;
         InputInterpreterInterface interpreter = new InputInterpreter();
@@ -16,12 +18,13 @@ class InputParser {
             //variable declaration
             String[] parts = s.split("=");
             //adding to list of instantiated variables
-            String variableName = sanitize(parts[0]);
+            this.variableName = sanitize(parts[0]);
             //Structure struc = new Structure(variableName, parseFunction(sanitize(parts[1])));
             //elements.add(struc);
-            func = "MING " + variableName + " = " + parseFunction(sanitize(parts[1]));
+            func = "Structure " + this.variableName + " = " + parseFunction(sanitize(parts[1]));
         } else {
             //not variable declaration, just function application
+            this.variableName = null;
             func = parseFunction(s);
         }
         return func;
@@ -31,16 +34,16 @@ class InputParser {
         String func;
         String[] function = s.split("\\(");
         if(function.length != 2) throw new InvalidFunctionException("Invalid function form");
-        String functionName = function[0];
-        String[] args  = function[1].split("\\)");
-        if (args.length != 1 && args[0].equals(function[1])) throw new InvalidFunctionException("Invalid function form");
-        args = Arrays.stream(args[0].split(",")).map(InputParser::sanitize).toArray(String[]::new); //trims whitespace
+        String functionName = function[0].toLowerCase();
+        String[] args = obtainArguments(function[1]);
 
         switch(functionName){
-            case "make-ring":           func = s;
+            case "make-ring":           if (args.length != 2) throw new InvalidFunctionException("make-ring requires 2 argument, given " + args.length);
+                                        func = "make_ring(" + (this.variableName != null ? this.variableName + "," : "") + s.substring(s.indexOf("(") + 1, s.indexOf(")")) + ");";
                                         break;
             // Making a linear molecular chain structure
-            case "make-chain":          func = s;
+            case "make-chain":          if (args.length != 1) throw new InvalidFunctionException("make-chain requires 1 argument, given " + args.length);
+                                        func = "make_chain(" + (this.variableName != null ? this.variableName + "," : "") + s.substring(s.indexOf("(") + 1, s.indexOf(")")) + ");";
                                         break;
             case "single-bond":         if (args.length != 2) throw new InvalidFunctionException("single-bond requires 2 arguments, " +
                                         "given " + args.length);
@@ -62,16 +65,22 @@ class InputParser {
                                         "given " + args.length);
                                         func = "bond(" + args[0] + ", " + args[1] + ", 5);";
                                         break;
-            case "mirror":              func = s;
+            case "mirror":              if (args.length != 1) throw new InvalidFunctionException("mirror requires 1 argument, given " + args.length);
+                                        func = s;
                                         break;
-            case "conver-to-smiles":    func = s;
+            case "convert-to-smiles":   if (args.length != 1) throw new InvalidFunctionException("convert-to-smiles requires 1 argument, given " + args.length);
+                                        func = "toSmiles(" + args[0] + ");";
                                         break;
-            case "check-validity":      func = s;
+            case "check-validity":      if (args.length != 1) throw new InvalidFunctionException("check-validity requires 1 argument, given " + args.length);
+                                        func = "checkValidity(" + args[0] + ");";
                                         break;
-            case "render":              func = s;
+            case "render":              if (args.length != 1) throw new InvalidFunctionException("render requires 1 argument, given " + args.length);
+                                        func = s;
                                         break;
             // Making a single-element molecular structure
-            case "make":                func = "make(" + s.substring(s.indexOf("(") + 1, s.indexOf(")")) + ")";
+            case "make":
+                                        if (args.length != 1) throw new InvalidFunctionException("make requires 1 argument, given " + args.length);
+                                        func = "make(" + (this.variableName != null ? this.variableName + "," : "") + s.substring(s.indexOf("(") + 1, s.indexOf(")")) + ");";
                                         break;
             //error because what is being use isn't in our language
             default:                    func = "ERROR"; //TODO: should throw exception? or ignore "ERROR"
@@ -80,10 +89,21 @@ class InputParser {
         return func;
     }
 
+    static String[] obtainArguments(String function) throws InvalidFunctionException {
+        String[] args  = function.split("\\)");
+        if (args.length != 1 && args[0].equals(function)) throw new InvalidFunctionException("Invalid function form");
+        args = Arrays.stream(args[0].split(",")).map(InputParser::sanitize).toArray(String[]::new); //trims whitespace
+        return args;
+    }
+
 
     //get rids of spaces in a string
-    private static String sanitize(String s){
+    static String sanitize(String s){
         return s.replaceAll("\\s+","");
+    }
+
+    static String stripOutStructure(String s) {
+        return s.replace("Structure", "");
     }
 
     /*
